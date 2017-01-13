e4962256a77e1c063190bf263c76ed5596b759be
Adrian Pieper <adrpieper@gmail.com>
2017-01-13 15:46:18 +0100

    Oprogramowałem przyciski

--- app/src/main/java/pl/pieper/adrian/animexample/MainActivity.java
+++ app/src/main/java/pl/pieper/adrian/animexample/MainActivity.java
@@ -1,7 +1,10 @@
 package pl.pieper.adrian.animexample;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button firstFragmentButton = (Button) findViewById(R.id.first_fragment_button);
+        Button secondFragmentButton = (Button) findViewById(R.id.second_fragment_button);
+        firstFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.empty_frame, new FirstFragment());
+                transaction.commit();
+            }
+        });
+        secondFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.empty_frame, new SecondFragment());
+                transaction.commit();
+            }
+        });
     }
 }
